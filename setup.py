4ab74e7f89632b4c4401e3476f87d2bc5f80f57e
Kiran Jonnalagadda <kiran@hasgeek.com>
2017-06-27 16:07:35 +0530

    Add Py3 topic for PyPI.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
